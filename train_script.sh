d16571e77a27e338f4982bb824bd99369f304392
Maxime Gloesener <max.gleu@gmail.com>
2025-03-14 15:33:44 +0100

    add

--- train_script.sh
+++ train_script.sh
@@ -1,5 +1,11 @@
-python train.py --model yolov11n
+python train.py --model yolo11n
 python train.py --model yolo11s
 python train.py --model yolo11m
 python train.py --model yolo11l
 python train.py --model yolo11x
+
+python train.py --model yolov10n
+python train.py --model yolov10s
+python train.py --model yolov10m
+python train.py --model yolov10l
+python train.py --model yolov10x
